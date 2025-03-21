a9511ffaaa22769f93487a8c12f88f06d1e109fe
Dylo98 <dawid.dyl@microsoft.wsei.edu.pl>
2025-03-21 19:19:09 +0100

    Add AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:expense_tracker/widgets/expenses.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      home: const Expenses(),
     ),
   );
 }
